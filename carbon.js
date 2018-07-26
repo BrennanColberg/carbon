78167b126ef01807abea3186b7d561b01de1d222
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-26 13:35:44 -0700

    JSON validation testing elements
    simple thing was made to display compacted JSON at index.html; no significance to function whatsoever

--- carbon.js
+++ carbon.js
@@ -3,6 +3,10 @@
 	
 	window.addEventListener("load", function() {
 		
+		ajaxGET("stations.json", function(json) {
+			qs("body").textContent = JSON.stringify(JSON.parse(json));
+		});
+		
 	});
 	
 }();
